0c5c8a61e7674a4ff0074da1c1ccc53d8ab57030
Nalivator3000 <nalivator3000@gmail.com>
2022-10-13 16:57:57 +0800

    Moved postgres' connect to main

--- scr/main.py
+++ scr/main.py
@@ -1,9 +1,21 @@
-import uvicorn as uvicorn
+from databases import Database
 from fastapi import FastAPI
 
 app = FastAPI()
+
+db = Database('postgresql+psycopg2://postgres:root@localhost/postgres_db')
 
 
 @app.get('/')
 def index():
     return {"status": "Working"}
+
+
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
--- scr/postgres_db.py
+++
@@ -1,5 +0,0 @@
-from databases import Database
-
-database = Database('postgresql+psycopg2://postgres:root@localhost/postgres_db')
-
-await database.connect()
