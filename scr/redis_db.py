c662be5b93e401df92315d91d7d45ed4a5b7ae5f
Nalivator3000 <nalivator3000@gmail.com>
2022-10-13 20:53:32 +0800

    add dotenv

--- scr/redis_db.py
+++ scr/redis_db.py
@@ -1,8 +1,13 @@
 import aioredis
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+REDIS_HOST = os.getenv('REDIS_HOST')
 
 
 async def main():
-    redis = aioredis.from_url("redis://localhost")
+    redis = aioredis.from_url("redis://REDIS_HOST")
     await redis.set("my-key", "value")
     value = await redis.get("my-key")
-    print(value)
