d6adc19b0113652621d3ac1449b858997fb1af6e
Nalivator3000 <nalivator3000@gmail.com>
2022-10-19 01:22:08 +0800

    Update schemas, add routes for users

--- app/base/models.py
+++ app/base/models.py
@@ -5,12 +5,15 @@ from sqlalchemy.sql import func
 Base = declarative_base()
 
 
-class User(Base):
+class DbUser(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)
-    bio = Column(String, default=None)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    bio = Column(String, default=None, nullable=True)
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+users = DbUser.__table__
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,3 +1,59 @@
-from ..base.schemas import User
-from ..base.base import db
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
+from base.schemas import *
+from base.models import users
+from base.base import database
+from typing import List
+
+router = APIRouter(
+    prefix='/user',
+    tags=['user']
+)
+
+
+@router.post('/signup', response_model=SignUpRequestModel)
+async def sign_up_user(u: SignUpRequestModel):
+    query = users.insert().values(
+        name=u.name,
+        email=u.email,
+        password=u.password,
+        bio=u.bio
+    )
+    record_id = await database.execute(query)
+    query = users.select().where(users.c.id == record_id)
+    row = await database.fetch_one(query)
+    return {**row}
+
+
+@router.get('/{id}', response_model=UserDisplay)
+async def get_user_by_id(id: int):
+    query = users.select().where(users.c.id == id)
+    user = await database.fetch_one(query)
+    return {**user}
+
+
+@router.get('/', response_model=List[UserDisplay])
+async def get_all_users():
+    query = users.select()
+    all_get = await database.fetch_all(query)
+    return all_get
+
+
+@router.put('/{id}', response_model=UserUpdateRequestModel)
+async def update_user(id: int, u: UserUpdateRequestModel):
+    query = users.update().where(users.c.id == id).values(
+        name=u.name,
+        email=u.email,
+        bio=u.bio
+    )
+    record_id = await database.execute(query)
+    query = users.select().where(users.c.id == record_id)
+    row = await database.fetch_one(query)
+    return {**row}
+
+
+@router.delete("/{id}") #response_model=User
+async def delete(id: int):
+    query = users.delete().where(users.c.id == id)
+    await database.execute(query)
+    return {'status': f'User {id} deleted successfully'}
+
