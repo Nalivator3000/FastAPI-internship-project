a24f7eea203e055482b1d6a6b531df17bbd14cfc
Nalivator3000 <nalivator3000@gmail.com>
2022-10-20 18:44:30 +0800

    Add models in routers, add class UserCRUD

--- app/routers/user.py
+++ app/routers/user.py
@@ -13,24 +13,24 @@ router = APIRouter(
 
 @router.post('/signup', response_model=SignUpRequestModel, status_code=status.HTTP_201_CREATED)
 async def sign_up_user(u: SignUpRequestModel) -> SignUpRequestModel:
-    return await user_services.sign_up_user(u)
+    return await user_services.UserCRUD.sign_up_user(u)
 
 
-@router.get('/{id}')
+@router.get('/{id}', response_model=UserDisplayWithId)
 async def get_user_by_id(id: int, response: Response) -> UserDisplayWithId:
-    return await user_services.get_user_by_id(id, response)
+    return await user_services.UserCRUD.get_user_by_id(id, response)
 
 
 @router.get('/', response_model=List[UserDisplayWithId], status_code=status.HTTP_200_OK)
 async def get_all_users() -> List[UserDisplayWithId]:
-    return await user_services.get_all_users()
+    return await user_services.UserCRUD.get_all_users()
 
 
-@router.put('/{id}')
+@router.put('/{id}', response_model=UserUpdateRequestModel)
 async def update_user(id: int, u: UserUpdateRequestModel, response: Response) -> UserUpdateRequestModel:
-    return await user_services.update_user(id, u, response)
+    return await user_services.UserCRUD.update_user(id, u, response)
 
 
 @router.delete("/{id}")
 async def delete_user(id: int, response: Response):
-    return await user_services.delete_user(id, response)
+    return await user_services.UserCRUD.delete_user(id, response)
--- app/services/user_services.py
+++ app/services/user_services.py
@@ -6,60 +6,58 @@ from fastapi import status, Response, HTTPException
 from typing import List
 
 
-async def sign_up_user(u) -> SignUpRequestModel:
-    query = users.insert().values(
-        name=u.name,
-        email=u.email,
-        password=Hash.bcrypt(u.password),
-        bio=u.bio
-    )
+class UserCRUD:
+    def __init__(self):
+        self.database = database
 
-    record_id = await database.execute(query)
-    query = users.select().where(users.c.id == record_id)
-    row = await database.fetch_one(query)
-
-    return SignUpRequestModel(**row)
-
-
-async def get_user_by_id(id: int, response: Response) -> UserDisplayWithId:
-    user = await database.fetch_one(users.select().where(users.c.id == id))
-    if user is not None:
-        response.status_code = status.HTTP_200_OK
-        user = await database.fetch_one(users.select().where(users.c.id == id))
-        return UserDisplayWithId(**user)
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
-
-
-async def get_all_users() -> List[UserDisplayWithId]:
-    user_list = await database.fetch_all(query=users.select())
-    return user_list
-
-
-async def update_user(id: int, u: UserUpdateRequestModel, response: Response)\
-        -> UserUpdateRequestModel or dict[str:str]:
-    user = await database.fetch_one(users.select().where(users.c.id == id))
-    if user is not None:
-        query = users.update().where(users.c.id == id).values(
+    async def sign_up_user(u) -> SignUpRequestModel:
+        query = users.insert().values(
             name=u.name,
             email=u.email,
-            bio=u.bio,
+            password=Hash.bcrypt(u.password),
+            bio=u.bio
         )
-        response.status_code = status.HTTP_200_OK
-        query = users.select().where(users.c.id == id)
+
+        record_id = await database.execute(query)
+        query = users.select().where(users.c.id == record_id)
         row = await database.fetch_one(query)
-        print('-\n-\n-\n-\n-\n', {**row}, '-\n-\n-\n-\n-\n')
-        return UserUpdateRequestModel(**row)
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
 
+        return SignUpRequestModel(**row)
 
-async def delete_user(id: int, response: Response):
-    user = await database.fetch_one(users.select().where(users.c.id == id))
-    if user is not None:
-        response.status_code = status.HTTP_200_OK
-        query = users.delete().where(users.c.id == id)
-        await database.execute(query)
-        return {'status': f'User {id} deleted successfully'}
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
+    async def get_user_by_id(id: int, response: Response) -> UserDisplayWithId:
+        user = await database.fetch_one(users.select().where(users.c.id == id))
+        if user is not None:
+            response.status_code = status.HTTP_200_OK
+            user = await database.fetch_one(users.select().where(users.c.id == id))
+            return UserDisplayWithId(**user)
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
+
+    async def get_all_users() -> List[UserDisplayWithId]:
+        user_list = await database.fetch_all(query=users.select())
+        return user_list
+
+    async def update_user(id: int, u: UserUpdateRequestModel, response: Response) -> UserUpdateRequestModel:
+        user = await database.fetch_one(users.select().where(users.c.id == id))
+        if user is not None:
+            users.update().where(users.c.id == id).values(
+                name=u.name,
+                email=u.email,
+                bio=u.bio,
+            )
+            response.status_code = status.HTTP_200_OK
+            query = users.select().where(users.c.id == id)
+            row = await database.fetch_one(query)
+            return UserUpdateRequestModel(**row)
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
+
+    async def delete_user(id: int, response: Response):
+        user = await database.fetch_one(users.select().where(users.c.id == id))
+        if user is not None:
+            response.status_code = status.HTTP_200_OK
+            query = users.delete().where(users.c.id == id)
+            await database.execute(query)
+            raise HTTPException(status_code=status.HTTP_201_CREATED, detail=f'User {id} deleted successfully')
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
